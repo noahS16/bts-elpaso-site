37444467e7e571a173653b1e69eb6ca5895c93c4
Noah Saenz <noahsaenz@Mac-mini.local>
2026-02-11 14:13:29 -0700

    Updated header layout

--- utils/scripts/armymap.js
+++ utils/scripts/armymap.js
@@ -84,8 +84,8 @@ searchInput.addEventListener('input', debounce(async () => {
         li.textContent = getPlaceLabel(place);
 
         li.onclick = async () => {
-            if (localStorage.getItem('cityAdded')) { console.log("You have already added a city this session."); showCityExistsModal(); }
-            else {
+            //if (localStorage.getItem('cityAdded')) { console.log("You have already added a city this session."); showCityExistsModal(); }
+            if(true) {
                 searchInput.value = getPlaceLabel(place);
                 await addCity(place);
                 localStorage.setItem('cityAdded', 'true');
